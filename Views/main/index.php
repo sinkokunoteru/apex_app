946e370f6de57bdfa5f74368bcf0eaf60bdf127f
sinkokunoteru <zpopfj@gmail.com>
2022-03-15 00:39:27 +0900

    メインページデザイン作成

--- Views/main/index.php
+++ Views/main/index.php
@@ -1,35 +1,96 @@
-
-<!DOCTYPE html>
+<!DOCTYPEhtml>
 <html lang="ja">
- <head>
- <meta charset="utf-8">
- <title>apex_legends</title>
- <meta name="description" content="腕前が近い人間とマッチング">
- <meta name="keywords" content="apex,apex legends,fps,マッチング,アプリ">
- <link rel="stylesheet" href="index.css">
+<head>
+<meta charset="utf-8">
+<title>apex_legends</title>
+<meta name="description" content="腕前が近い人間とマッチング">
+<meta name="keywords" content="apex,apexlegends,fps,マッチング,アプリ">
+<link rel="stylesheet" href="../css/main.css">
+</head>
 
- <script src="sample.js"></script>
- </head>
+<body>
+  <header>
+    <nav>
+      <ul class="header_group">
+        <li>Legends_match</li>
+        <li>Service</li>
+        <li>Function</li>
+        <li>Price</li>
+        <li><a href="../register/login.php">Login</a></li>
+      </ul>
+    </nav>
 
- <body>
- <!----- ヘッダー ----->
- <header>ヘッダー</header>
- <nav>ナビ</nav>
- <!----- ヘッダー END ----->
+    <h1 class="title_word">Game_matching_system</h1>
+    <div class="register_button">
+      <a class="register_link" href="../register/sign_up.php">新規登録</a>
+    </div>
+  </header>
 
- <!----- メインコンテンツ ----->
- <article>
- <h1>腕前が近い人とマッチングしよう</h1>
- <section>
- <h2></h2>
- <p><a href="../register/sign_up.php">会員登録がまだの方はこちら</a></p>
- <p><a href="../register/login.php">ログインはこちら</a></p>
- </section>
- </article>
- <!----- メインコンテンツ END ----->
-
- <!----- フッター ----->
- <footer>フッター</footer>
- <!----- フッター END ----->
- </body>
+  <section class="sample_user_info">
+    <!-- タイムライン上のホットな動画をいい感じに表示かな -->
+    <div class="text_block">
+      <p id="text_block_header">バンガロール最強</p>
+      <p id="text_contents">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    </div>
+    <div class="text_block">
+      <p id="text_block_header">バンガロール最強</p>
+      <p id="text_contents">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    </div>
+    <div class="text_block">
+      <p id="text_block_header">バンガロール最強</p>
+      <p id="text_contents">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    </div>
+    <div class="text_block">
+      <p id="text_block_header">バンガロール最強</p>
+      <p id="text_contents">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+    </div>
+  </section>
+  <footer>
+    <div class="foot_information">
+      <ul>
+        <li class="info_top">法的事項</li>
+        <li>プライバシーポリシー</li>
+        <li>クッキーポリシー</li>
+        <li>知的財産権</li>
+        <li>利用規約</li>
+      </ul>
+      <ul>
+        <li class="info_top">採用情報</li>
+        <li>採用ポータル</li>
+        <li>製作者ツイッター</li>
+      </ul>
+      <ul>
+        <li class="info_top">ソーシャルメディア</li>
+        <li>アイコン</li>
+        <li>アイコン</li>
+        <li>アイコン</li>
+        <li>アイコン</li>
+      </ul>
+      <ul>
+        <li class="info_top">よくある質問</li>
+        <li>目的地で出会いを探す</li>
+        <li>プレスルーム</li>
+        <li>お問い合わせ</li>
+        <li>プロモーション</li>
+      </ul>
+    </div>
+    <div class="information_line"></div>
+    <div class="foot_block">
+      <p class="app_link">アプリを入手!</p>
+      <p class="information_detail">出会いを探している皆さん。求めているのが恋人でも、友達でも、チャット相手でも、Tinderはあなたにぴったりの場所です。累計マッチ数550億以上のTinderなら、相性ぴったりの相手が見つかります。出会いのカタチは以前から大きく変わり、現在ではほとんどの人がオンラインで出会いを見つけています。世界で最も人気のある無料マッチングアプリ、Tinderを開けば、あなたと同じように出会いを求め</p><br>
+      <p class="information_detail">Tinderは、新しい人とつながりたいすべての人のための場所です。真剣に彼氏や彼女を見つけたい？お任せください。友達の輪を広げたい？たくさん見つかります。Tinderはただの出会い系サイトではありません。さまざまな背景や経験を持つ人たちが集まり、つながって、思い出をつくれる、あなたの世界が広がるアプリです。</p>
+    </div>  
+    <div class="information_line"></div>
+    <div class="foot_link">
+      <p>よくある質問</p>
+      <p>安全に使うためのコツ</p>
+      <p>利用規約</p>
+      <p>クッキーポリシー</p>
+      <p>プライバシーポリシー</p>
+    </div>
+    <div class="copy_right">
+      <p>© 2022 Match Group, LLC, All Rights Reserved.</p>
+    </div>
+  </footer>
+</body>
 </html>
